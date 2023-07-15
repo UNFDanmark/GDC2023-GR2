7e2a9fd7d83bd254276b3d8f6ebc154206435649
Deltager <teknisk+GDC2023-GR2@game.unf.dk>
2023-07-15 17:55:34 +0200

    Lavede accord check

--- TheUnityProject/Assets/Scripts/InstumentController.cs
+++ TheUnityProject/Assets/Scripts/InstumentController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,71 +13,96 @@ public class InstumentController : MonoBehaviour
     [SerializeField]
     Color[] colors;
 
-    List<int> latestNotes;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    float cooldownTime;
 
-    // Update is called once per frame
-    void Update()
-    {
-        NoteInput();
-    }
+    float timeLeft;
 
+    public List<int>[] AvailableChords;
+    public List<int> latestNotes = new List<int>();
+    
     void NoteInput() {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft <= 0)
+        {
+            latestNotes.Clear();
+        }
         if (Input.GetKeyDown("u"))
         {
             images[0].color = colors[0];
-            latestNotes.Add(0);
-            if(latestNotes.Count > 3)
-            {
-                Debug.Log("agfpiewhf");
-            }
+            AddToList(0);
         }
         if (Input.GetKeyUp("u")) { images[0].color = Color.white; }
 
         if (Input.GetKeyDown("i"))
         {
             images[1].color = colors[1];
+            AddToList(1);
         }
         if (Input.GetKeyUp("i")) { images[1].color = Color.white; }
 
         if (Input.GetKeyDown("o"))
         {
             images[2].color = colors[2];
+            AddToList(2);
         }
         if (Input.GetKeyUp("o")) { images[2].color = Color.white; }
 
         if (Input.GetKeyDown("p"))
         {
             images[3].color = colors[3];
+            AddToList(3);
         }
         if (Input.GetKeyUp("p")) { images[3].color = Color.white; }
 
         if (Input.GetKeyDown("j"))
         {
             images[4].color = colors[4];
+            AddToList(4);
         }
         if (Input.GetKeyUp("j")) { images[4].color = Color.white; }
 
         if (Input.GetKeyDown("k"))
         {
             images[5].color = colors[5];
+            AddToList(5);
         }
         if (Input.GetKeyUp("k")) { images[5].color = Color.white; }
 
         if (Input.GetKeyDown("l"))
         {
             images[6].color = colors[6];
+            AddToList(6);
         }
         if (Input.GetKeyUp("l")) { images[6].color = Color.white; }
 
         if (Input.GetKeyDown(KeyCode.Semicolon))
         {
             images[7].color = colors[7];
+            AddToList(7);
         }
         if (Input.GetKeyUp(KeyCode.Semicolon)) { images[7].color = Color.white; }
+
+        if (Input.GetKeyDown(KeyCode.RightShift)) { ChordCheck(); }
     }
+    void AddToList(int i)
+    {
+        latestNotes.Add(i);
+        if (latestNotes.Count > 3)
+        {
+            latestNotes.RemoveAt(0);
+        }
+        timeLeft = cooldownTime;
+        
+    }
+
+    void ChordCheck() {
+    
+    }
+
+    void Update()
+    {
+        NoteInput();
+    }
+
 }
